fec8262bdc764caee10c650b7a991d2b888b4519
Mena Ramy Enaeet <50600124+mena2512@users.noreply.github.com>
2024-11-27 22:49:40 +0300

    Add files via upload

---
+++ MetroApp/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form1))
+        Label1 = New Label()
+        cbStart = New ComboBox()
+        Label2 = New Label()
+        cbEnd = New ComboBox()
+        btnCalculate = New Button()
+        lblResult = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(10, 11)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(74, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Start Station:"
+        ' 
+        ' cbStart
+        ' 
+        cbStart.FormattingEnabled = True
+        cbStart.Location = New Point(98, 9)
+        cbStart.Margin = New Padding(3, 2, 3, 2)
+        cbStart.Name = "cbStart"
+        cbStart.Size = New Size(133, 23)
+        cbStart.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(10, 37)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(70, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "End Station:"
+        ' 
+        ' cbEnd
+        ' 
+        cbEnd.FormattingEnabled = True
+        cbEnd.Location = New Point(98, 34)
+        cbEnd.Margin = New Padding(3, 2, 3, 2)
+        cbEnd.Name = "cbEnd"
+        cbEnd.Size = New Size(133, 23)
+        cbEnd.TabIndex = 2
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        btnCalculate.Location = New Point(10, 130)
+        btnCalculate.Margin = New Padding(3, 2, 3, 2)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(82, 22)
+        btnCalculate.TabIndex = 4
+        btnCalculate.Text = "Calculate"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.Location = New Point(10, 58)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(80, 15)
+        lblResult.TabIndex = 5
+        lblResult.Text = "Shortest path:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(261, 9)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(115, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Inter Change Station"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 73)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(68, 15)
+        Label4.TabIndex = 7
+        Label4.Text = "Total time  :"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(96, 73)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 15)
+        Label5.TabIndex = 8
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(261, 34)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(0, 15)
+        Label6.TabIndex = 9
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(469, 160)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(lblResult)
+        Controls.Add(btnCalculate)
+        Controls.Add(Label2)
+        Controls.Add(cbEnd)
+        Controls.Add(Label1)
+        Controls.Add(cbStart)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Margin = New Padding(3, 2, 3, 2)
+        Name = "form1"
+        Text = "Metro CO"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cbStart As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cbEnd As ComboBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+
+End Class
---
+++ MetroApp/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Collections.Specialized.BitVector32
+
+Public Class form1
+    Private metroLines As New Dictionary(Of String, List(Of String))()
+    Private interchangeStations As New List(Of String)()
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Define metro lines
+        metroLines("Line 1") = New List(Of String) From {
+            "Helwan", "Ain Helwan", "Helwan University", "Wadi Hof", "Hadayek Helwan", "El-Maasara", "Tora El-Asmant",
+            "Kozzika", "Tora El-Balad", "Sakanat El-Maadi", "Maadi", "Hadayek El-Maadi", "Dar El-Salam", "El-Zahraa",
+            "Mar Girgis", "El-Malek El-Saleh", "Al-Sayeda Zeinab", "Saad Zaghloul", "Sadat", "Nasser", "Orabi",
+            "Al-Shohadaa", "Ghamra", "El-Demerdash", "Manshiet El-Sadr", "Kobri El-Qobba", "Hammamat El-Qobba",
+            "Saray El-Qobba", "Hadayeq El-Zaitoun", "Helmeyet El-Zaitoun", "El-Matareyya", "Ain Shams", "Ezbet El-Nakhl", "El-Marg", "New El-Marg"
+        }
+
+        metroLines("Line 2") = New List(Of String) From {
+            "Shubra El-Kheima", "Kolleyet El-Zeraa", "Mezallat", "Khalafawy", "Saint Teresa", "Rod El-Farag",
+            "Masarra", "Al-Shohadaa", "Attaba", "Sadat", "Dokki", "El-Behoos", "Cairo University", "Faisal", "Giza",
+            "Omm El-Masryeen", "Sakiat Mekki", "El-Monib"
+        }
+
+        metroLines("Line 3") = New List(Of String) From {
+            "Adly Mansour", "El-Haykestep", "Omar Ibn El-Khattab", "Qobaa", "Hesham Barakat", "El-Nozha", "Nadi El-Shams",
+            "Alf Maskan", "Heliopolis Square", "Haroun", "Al-Ahram", "Koleyet El-Banat", "Cairo Stadium", "Fair Zone",
+            "Abbassia", "Abdou Pasha", "El-Geish", "Bab El-Shaaria", "Attaba", "Nasser", "Maspero", "Safaa Hegazy", "Kit Kat"
+        }
+
+        ' Define interchange stations
+        interchangeStations = New List(Of String) From {"Al-Shohadaa", "Sadat", "Attaba", "Nasser"}
+
+        ' Add stations to dropdown lists
+        For Each lineStations In metroLines.Values
+            For Each station In lineStations
+                If Not cbStart.Items.Contains(station) Then cbStart.Items.Add(station)
+                If Not cbEnd.Items.Contains(station) Then cbEnd.Items.Add(station)
+            Next
+        Next
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If cbStart.SelectedItem Is Nothing OrElse cbEnd.SelectedItem Is Nothing Then
+            lblResult.Text = "Please select both start and end stations."
+            Return
+        End If
+
+        Dim startStation As String = cbStart.SelectedItem.ToString()
+        Dim endStation As String = cbEnd.SelectedItem.ToString()
+        Dim stationCount As Integer = CalculateShortestPath(startStation, endStation)
+        lblResult.Text = If(stationCount <> -1, $"Shortest path: {stationCount} stations.", "No path found between the selected stations.")
+        Dim interchanges As String = GetInterchangeStations(startStation, endStation)
+        Dim totalTime As Double = CalculateJourneyTime(startStation, endStation)
+        Label6.Text = interchanges
+        Label5.Text = totalTime
+
+    End Sub
+
+    Private Function CalculateShortestPath(startStation As String, endStation As String) As Integer
+        ' BFS to find the shortest path
+        Dim queue As New Queue(Of Tuple(Of String, Integer))()
+        Dim visitedStations As New HashSet(Of String)()
+
+        queue.Enqueue(Tuple.Create(startStation, 0))
+        visitedStations.Add(startStation)
+
+        While queue.Count > 0
+            Dim current = queue.Dequeue()
+            Dim currentStation = current.Item1
+            Dim currentCount = current.Item2
+
+            If currentStation = endStation Then
+                Return currentCount
+            End If
+
+            Dim neighbors = GetNeighbors(currentStation)
+            For Each neighbor In neighbors
+                If Not visitedStations.Contains(neighbor) Then
+                    visitedStations.Add(neighbor)
+                    queue.Enqueue(Tuple.Create(neighbor, currentCount + 1))
+                End If
+            Next
+        End While
+
+        Return -1 ' No path found
+    End Function
+
+    Private Function GetNeighbors(station As String) As List(Of String)
+        Dim neighbors As New List(Of String)()
+
+        ' Get adjacent stations on the same line
+        For Each line In metroLines.Values
+            If line.Contains(station) Then
+                Dim index = line.IndexOf(station)
+                If index > 0 Then neighbors.Add(line(index - 1))
+                If index < line.Count - 1 Then neighbors.Add(line(index + 1))
+            End If
+        Next
+
+        ' Add interchange station neighbors
+        If interchangeStations.Contains(station) Then
+            For Each line In metroLines.Values
+                If line.Contains(station) Then
+                    Dim index = line.IndexOf(station)
+                    If index > 0 Then neighbors.Add(line(index - 1))
+                    If index < line.Count - 1 Then neighbors.Add(line(index + 1))
+                End If
+            Next
+        End If
+
+        Return neighbors.Distinct().ToList() ' Remove duplicates
+
+    End Function
+    Private Function GetInterchangeStations(startStation As String, endStation As String) As String
+        ' BFS to find the shortest path and track interchange stations
+        Dim queue As New Queue(Of Tuple(Of String, List(Of String)))()
+        Dim visitedStations As New HashSet(Of String)()
+
+        ' Enqueue the start station with an empty list of visited interchange stations
+        queue.Enqueue(Tuple.Create(startStation, New List(Of String)()))
+        visitedStations.Add(startStation)
+
+        While queue.Count > 0
+            Dim current = queue.Dequeue()
+            Dim currentStation = current.Item1
+            Dim interchangePath = current.Item2
+
+            ' If the current station is the end station, return the interchange stations as a string
+            If currentStation = endStation Then
+                Return If(interchangePath.Any(), String.Join(", ", interchangePath), "No interchanges needed.")
+            End If
+
+            Dim neighbors = GetNeighbors(currentStation)
+            For Each neighbor In neighbors
+                If Not visitedStations.Contains(neighbor) Then
+                    visitedStations.Add(neighbor)
+
+                    ' Clone the current interchange path
+                    Dim newInterchangePath = New List(Of String)(interchangePath)
+
+                    ' Add the station to the path if it's an interchange station
+                    If interchangeStations.Contains(currentStation) AndAlso Not newInterchangePath.Contains(currentStation) Then
+                        newInterchangePath.Add(currentStation)
+                    End If
+
+                    queue.Enqueue(Tuple.Create(neighbor, newInterchangePath))
+                End If
+            Next
+        End While
+
+        Return "No path found between the selected stations."
+    End Function
+    Private Function CalculateJourneyTime(startStation As String, endStation As String) As String
+        ' Average time between two stations (in minutes)
+        Const averageTimePerStation As Double = 3
+
+        ' Calculate the shortest path in terms of station count
+        Dim stationCount As Integer = CalculateShortestPath(startStation, endStation)
+
+        ' If no path is found, return an appropriate message
+        If stationCount = -1 Then
+            Return "No path found between the selected stations."
+        End If
+
+        ' Calculate the total time for the journey
+        Dim totalTime As Double = stationCount * averageTimePerStation
+
+        Return totalTime
+    End Function
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
